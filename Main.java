5ce837fa972a7e872670cf467c429d29f1254b96
Alfred Mokgohloa <45788338+DeadMan-Moc@users.noreply.github.com>
2019-06-25 16:39:43 +0200

    Add files via upload
    updated

---
+++ Main.java
@@ -0,0 +1,18 @@
+package  sources.deadman;
+export 
+
+public class Main {
+    public static void main (String[] arg ) throws myErrors {
+        if (arg.length == 1) {
+            parser.parse(arg[0], weatherTower);
+            WeatherTower weatherTower = new weatherTower();
+        }
+        try {
+
+        }
+        catch (Exception e) {
+                //TODO: handle exception
+
+        }
+    }
+}
