e6cadb2c75fb5b0f7e377f38cbe0b998ccbe752c
DeadMan-MOC <mokgohloallie@gmail.com>
2019-07-02 10:07:45 +0200

    weather services not serviced

--- WeatherProvider.java
+++ WeatherProvider.java
@@ -2,17 +2,20 @@ package sources.deadman;
 
 public class WeatherProvider {
     private static WeatherProvider weatherProvider;
-    private static String[] weather;
+    private static String[] weather = {"SUN", "RAIN", "FOG", "SNOW"};
 
     private WeatherProvider(){
-
+        /*;;;*/
     }
 
     public static WeatherProvider getProvider() {
-
+        if (weatherProvider == null) {
+            weatherProvider = new WeatherProvider();
+        }
+        return weatherProvider;
     }
 
     public String getCurrentWeather(Coordinates coordinates) {
-
+        
     }
 }
