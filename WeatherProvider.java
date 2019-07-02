a72c2ddec828b3f28eb2f86f2a8e7e64a270718b
DeadMan-MOC <mokgohloallie@gmail.com>
2019-07-02 10:19:16 +0200

    errors

--- WeatherProvider.java
+++ WeatherProvider.java
@@ -16,6 +16,6 @@ public class WeatherProvider {
     }
 
     public String getCurrentWeather(Coordinates coordinates) {
-        
+        /* .. */
     }
 }
--- WeatherTower.java
+++ WeatherTower.java
@@ -2,10 +2,10 @@ package sources.deadman;
 
 public class WeatherTower extends Tower {
     public String getWeather(Coordinates coordinates) {
-
+        return(getWeather().)
     }
 
     void changeWeather(){
-
+        this.conditionsChanged;
     }
 }
